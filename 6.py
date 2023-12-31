8b76679540da2c26873751837227c7c74d98ddd8
TaRa1999R <rezaietara@gmail.com>
2023-12-31 15:24:22 +0330

    assignment29

---
+++ 6.py
@@ -0,0 +1,11 @@
+
+import cv2
+
+room = cv2.imread ("inputs\input_6_room.jpg")
+floor = cv2.imread ("inputs\input_6_flore.jpg")
+mask = cv2.imread ("inputs\input_6_mask.jpg")
+
+masked_floor = 
+
+cv2.imshow ("Virtual Decoration" , )
+cv2.waitKey ()
