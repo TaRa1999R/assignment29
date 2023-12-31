2a519d758058a6a26f1d4a91ac1c1701df38f7bc
TaRa1999R <rezaietara@gmail.com>
2023-12-31 12:49:11 +0330

    assignment29

--- 4.py
+++ 4.py
@@ -4,8 +4,20 @@ import cv2
 full_text = cv2.imread ("inputs\input_4_full.png")
 code_breaker = cv2.imread ("inputs\input_4_check.png")
 
-full_text = 
+for row in range (full_text.shape[1]) :
+    for col in range (full_text.shape[0]) :
+        full_text[row][col] = 255 - full_text[row][col]
+
+for row in range (code_breaker.shape[1]) :
+    for col in range (code_breaker.shape[0]) :
+        code_breaker[row][col] = 255 - code_breaker[row][col]
+
 secret_text = cv2.subtract (full_text , code_breaker)
+
+for row in range (secret_text.shape[1]) :
+    for col in range (secret_text.shape[0]) :
+        secret_text[row][col] = secret_text[row][col] + 256
 
 cv2.imshow ("Secret Text" , secret_text)
 cv2.waitKey ()
+cv2.imwrite ("outputs\output_4_secret_text.png" , secret_text)
