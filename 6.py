775ec08a1d34d9011c00dab3e8e8decf9cf59f22
TaRa1999R <rezaietara@gmail.com>
2023-12-31 18:50:03 +0330

    assignment29

--- 6.py
+++ 6.py
@@ -2,10 +2,11 @@
 import cv2
 
 room = cv2.imread ("inputs\input_6_room.jpg")
-floor = cv2.imread ("inputs\input_6_flore.jpg")
+floor = cv2.imread ("inputs\input_6_floor.jpg")
 mask = cv2.imread ("inputs\input_6_mask.jpg")
 
-masked_floor = 
+mask = mask / 255.0
+masked_floor = floor * mask
 
-cv2.imshow ("Virtual Decoration" , )
+cv2.imshow ("Virtual Decoration" , masked_floor)
 cv2.waitKey ()
