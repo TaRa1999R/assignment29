15db94d73c011f0834ef583ba4ebcc5b231890d2
TaRa1999R <rezaietara@gmail.com>
2023-12-31 15:15:14 +0330

    assignment29

---
+++ 3.py
@@ -0,0 +1,15 @@
+
+import cv2
+
+img = cv2.imread ("inputs\input_face.jpg" , 0)
+
+inverted = 255 - img
+blurred = cv2.GaussianBlur (inverted , (21,21) , 0)
+inverted_blurred = 255 - blurred
+
+sketch = img / inverted_blurred
+sketch = sketch * 255
+
+cv2.imshow ("Sketch picture" , sketch)
+cv2.waitKey ()
+cv2.imwrite ("outputs\output_3_photo_to_sketch.jpg" , sketch)
